97e3a7749248dd0d23dee2c53ab850d0d1537d09
Yanjie He <heyanjie0@outlook.com>
2021-01-25 11:10:11 -0500

    split

--- include/pystr.hpp
+++ include/pystr.hpp
@@ -2,5 +2,40 @@
 #define PY_STR_HPP
 
 #include <string>
+#include <vector>
+
+namespace pystr {
+
+using std::vector;
+using std::basic_string;
+
+template <class CharType>
+inline vector<basic_string<CharType>>
+split(const basic_string<CharType> &s, const basic_string<CharType> &separator,
+      int maxsplit = -1) {
+  basic_string<CharType>::size_type p1 = 0;
+  basic_string<CharType>::size_type p2 = 0;
+  vector<basic_string<CharType>> result;
+  int n_splits = 0;
+
+  p2 = s.find(separator, p1);
+  while (p2 != basic_string<CharType>::npos) {
+    if (maxsplit != -1) {
+      if (n_splits >= maxsplit) {
+        result.push_back(s.substr(p1));
+        return result;
+      } else {
+        n_splits++;
+      }
+    }
+    result.push_back(s.substr(p1, p2 - p1));
+    p1 = p2 + separator.size();
+    p2 = s.find(separator, p1);
+  }
+  result.push_back(s.substr(p1));
+  return result;
+}
+
+}; // namespace pystr
 
 #endif // PY_STR_HPP
--- test/test.cpp
+++ test/test.cpp
@@ -1,2 +1,30 @@
 #define CATCH_CONFIG_MAIN
 #include <catch2/catch.hpp>
+#include <pprint.hpp>
+#include "pystr.hpp"
+
+using std::vector;
+using std::string;
+
+TEST_CASE("split", "[pystr::split]") {
+  pprint::PrettyPrinter printer;
+  auto res = pystr::split<char>("a::b::c", "::", 1);
+  printer.print("size = ", res.size(), "values = ", res);
+  REQUIRE(pystr::split<char>("a, b, c", ", ") == vector<string>{"a", "b", "c"});
+  REQUIRE(pystr::split<char>("", "/", 1) == vector<string>{""});
+  REQUIRE(pystr::split<char>("/", "/", 1) == vector<string>{"", ""});
+  REQUIRE(pystr::split<char>(" ", " ", 1) == vector<string>{"", ""});
+  REQUIRE(pystr::split<char>(" /", "/", 1) == vector<string>{" ", ""});
+  REQUIRE(pystr::split<char>(" //", "/", 1) == vector<string>{" ", "/"});
+  REQUIRE(pystr::split<char>("a  ", " ", 1) == vector<string>{"a", " "});
+  REQUIRE(pystr::split<char>("//as//rew//gdf", "//") ==
+          vector<string>{"", "as", "rew", "gdf"});
+  REQUIRE(pystr::split<char>("/root/world", "/", 0) ==
+          vector<string>{"/root/world"});
+  REQUIRE(pystr::split<char>("/root/world", "/", 1) ==
+          vector<string>{"", "root/world"});
+  REQUIRE(pystr::split<char>("/root/world", "/", 2) ==
+          vector<string>{"", "root", "world"});
+  REQUIRE(pystr::split<char>("/root/world", "/", -1) ==
+          vector<string>{"", "root", "world"});
+}
